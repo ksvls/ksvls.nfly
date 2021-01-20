a8ee7a1a902abd463beeb0fac533527121cc52bd
Danmiii <fahmih774@gmail.com>
2021-01-20 12:19:48 +0700

    Initial commit.

---
+++ pages/__app.tsx
@@ -0,0 +1,6 @@
+import '../styles/global.css'
+
+
+export default ({Component, pageProps})=>{
+    return <Component {...pageProps} />
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default ()=>{
+    return (
+        <div>
+            <h1>Not Done Yet</h1>
+        </div>
+    )
+}
